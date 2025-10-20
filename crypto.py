66e86ab81ec49c3c64e2d387264ae0152d4312d8
Afonso Carvalho <afonsomiguelcarvalho2006@gmail.com>
2025-10-20 14:47:36 +0100

    Added key generation

--- crypto.py
+++ crypto.py
@@ -1,5 +1,10 @@
 import hashlib
 
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
+
 def hash_password(password: str) -> str:
     """ Hashes a password using SHA-256"""
     return hashlib.sha256(password.encode()).hexdigest()
@@ -7,3 +12,16 @@ def hash_password(password: str) -> str:
 def verify_master_password(entered_pass: str, stored_hash: str) -> bool:
     hashed_input = hash_password(entered_pass)
     return hashed_input == stored_hash
+
+def generatekey(master_pass: str) -> bytes:
+    salt = b'password_manager_salt_2025'
+    # FOR NOW -- I use a fixed salt, in actual production ima generate a random salt and store it
+
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000, #Kinda high, ill adjust later
+    )
+    key=base64.urlsafe_b64encode(kdf.derive(master_pass.encode()))
+    return key
